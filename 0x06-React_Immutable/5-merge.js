7328ec4a48e6a65f7852a8924f9bfb7c29830e83
hamzadev2010 <hamzachanik@outlook.com>
2024-05-30 03:50:55 -0700

    task

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,4 +1,4 @@
-//a function named concatElements
+// a function named concatElements
 import { List, Map } from 'immutable';
 
 export const concatElements = (page1, page2) => List(page1).concat(List(page2));
